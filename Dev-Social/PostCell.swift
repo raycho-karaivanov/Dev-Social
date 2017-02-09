d3b81d57ff11e92b5e6bd4974d00918573d2e782
raycho-karaivanov <raycho.karaivanov@gmail.com>
2017-02-09 22:30:38 +0200

    Display of some info from the Firebase DB to the UI - DONE

--- Dev-Social/PostCell.swift
+++ Dev-Social/PostCell.swift
@@ -16,6 +16,8 @@ class PostCell: UITableViewCell {
     @IBOutlet weak var caption: UITextView!
     @IBOutlet weak var likesLbl: UILabel!
     
+    var post: Post!
+    
     
     override func awakeFromNib() {
         super.awakeFromNib()
@@ -23,6 +25,12 @@ class PostCell: UITableViewCell {
   
     }
 
-    
+    func configureCell(post: Post) {
+        self.post = post
+        
+        self.caption.text = post.caption
+        self.likesLbl.text = "\(post.likes)"
+        
+    }
 
 }
