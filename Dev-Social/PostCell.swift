8526b7542c74f2e1e4f36206f8f4f47d88896624
raycho-karaivanov <raycho.karaivanov@gmail.com>
2017-02-09 13:16:24 +0200

    The Cells are showing now

---
+++ Dev-Social/PostCell.swift
@@ -0,0 +1,28 @@
+//
+//  PostCell.swift
+//  Dev-Social
+//
+//  Created by Raycho Karaivanov on 09/02/2017.
+//  Copyright © 2017 Raycho Karaivanov. All rights reserved.
+//
+
+import UIKit
+
+class PostCell: UITableViewCell {
+
+    @IBOutlet weak var profileImg: UIImageView!
+    @IBOutlet weak var usernameLbl: UILabel!
+    @IBOutlet weak var postImg: UIImageView!
+    @IBOutlet weak var caption: UITextView!
+    @IBOutlet weak var likesLbl: UILabel!
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+  
+    }
+
+    
+
+}
--- Dev-Social/RoundedCornersImage.swift
+++ Dev-Social/RoundedCornersImage.swift
@@ -1,5 +1,5 @@
 //
-//  CircleView.swift
+//  RoundedCornersImage.swift
 //  Dev-Social
 //
 //  Created by Raycho Karaivanov on 09/02/2017.
@@ -8,18 +8,15 @@
 
 import UIKit
 
-class CircleView: UIImageView {
+class RoundedCornersImage: UIImageView {
 
-    
-    
     override func layoutSubviews() {
-        
         layer.shadowColor = UIColor(red: SHADOW_GRAY, green: SHADOW_GRAY, blue: SHADOW_GRAY, alpha: 0.6).cgColor
         layer.shadowOpacity = 0.8
         layer.shadowRadius = 5.0
         layer.shadowOffset = CGSize(width: 1.0, height: 1.0)
-
-        layer.cornerRadius = self.frame.width / 2
+        layer.cornerRadius = 2.0
         clipsToBounds = true
     }
+
 }
